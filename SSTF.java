ec00232e3b3f7b5908b2d0cd24c6421021caeb5a
annebernadettebonuan <60252891+annebernadettebonuan@users.noreply.github.com>
2020-11-19 19:17:27 +0800

    Update SSTF.java

--- SSTF.java
+++ SSTF.java
@@ -1,5 +1,13 @@
+import java.util.Scanner;
+
 public class SSTF{
     public static void main(String[] args){
-        System.out.print("Hrllo");
+        System.out.println("Hrllo");
+
+        Scanner s = new Scanner(System.in);
+        int a = s.nextInt();
+
+        System.out.println(a);
+        s.close();
     }
 }
